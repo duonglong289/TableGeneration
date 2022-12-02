20f1c08d34f439dc611240585c29424c5d533db3
Long Duong <duonglong0298@gmail.com>
2022-12-02 09:21:06 +0000

    last generate script

--- generate.sh
+++ generate.sh
@@ -1,19 +1,19 @@
 python generate_data.py \
-    --num=10 \
+    --num=10000 \
     --output='output/simple_table' \
     --cell_box_type='cell' \
     --min_row=3 \
     --max_row=30 \
     --min_col=2 \
-    --max_col=8 \
+    --max_col=10 \
     --max_span_row_count=5 \
     --max_span_col_count=5 \
     --max_span_value=4 \
     --min_txt_len=2 \
     --max_txt_len=40 \
     --color_prob=0 \
-    --cell_max_width=200 \
+    --cell_max_width=220 \
     --cell_max_height=0 \
     --browser='chrome' \
     --border_type='full_line' \
-    --num_workers=0
+    --num_workers=8
